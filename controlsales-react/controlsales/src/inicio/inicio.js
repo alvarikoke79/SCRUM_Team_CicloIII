054b1de78b0828b0c95ebb6e09adabe26f41f0eb
antonio <45393639+JoseA94@users.noreply.github.com>
2021-10-07 21:52:12 -0500

    Intrerfaz de maestro de ventas y enlaces
    Se agrega la interfaz de maestro de ventas en la carpeta src, además se modifica el archivo App.js para realizar los enlaces con react-router.dom

---
+++ controlsales-react/controlsales/src/inicio/inicio.js
@@ -0,0 +1,53 @@
+
+import './inicio.css';
+import logo from './media/logo.jpg'
+import {Link} from 'react-router-dom'
+function Inicio() {
+    return (
+      <div id="main-container">
+  
+          <div id="containerlogueo">
+  
+              <div className="box">
+                  <Link to ='/'> <section className="logo">
+                      <img src={logo} alt ="logo" className="imgprincipal"/>
+                  </section></Link>
+                 
+                  <div className="modulo">
+                      <p>Modulo administrador de productos:</p>
+                      <div className="botonlat">
+                          <a href="">Registro de Productos</a>
+                      </div>
+                      <div className="botonlat">
+                          <a href="">Maestro de Productos</a>
+                      </div>
+                  </div>
+                  <div className="modulo">
+                      <p>Modulo administrador de ventas:</p>
+                      <div className="botonlat">
+                          <a href="">Registro de Ventas</a>
+                      </div>
+                      <div className="botonlatMV">
+                          <Link to = '/maestrodVentas'>Maestro de Ventas</Link>
+                      </div>
+                  </div>
+                  <div className="modulo1">
+                      <div className="botonlat">
+                          <a href="">Gestión de Usuarios</a>
+                      </div>
+                  </div>
+                  <div className="salir">
+                      <div className="botonlat1">
+                          <a href="">Cerrar Sesión</a>
+                      </div>
+                  </div>
+  
+  
+              </div>
+              
+          </div>
+      </div>
+    );
+  }
+  
+  export default Inicio;
