f71610599afeccdd0e10a2901058718faa2259f3
ALVARO RUIZ MACIAS <90515869+alvarikoke79@users.noreply.github.com>
2021-10-09 10:10:20 -0500

    Update App.jsx
    Cambios en el texto de la landing page.

--- controlsales/src/App.jsx
+++ controlsales/src/App.jsx
@@ -9,11 +9,11 @@ function App() {
         <div className="login">
         <div className="Landing">
           <span className="Txlanding">
-            Control sales te permite registrar, y gestionar los produtos de tu
-            tienda, tambien permite registrar y gestionar las ventas tanto
-            fisicas como virtuales. Si tu empresa ya esta usando control sales,
-            accede desde el boton de la derecha con tu cuenta de correo de
-            google.
+            Control Sales te permite gestionar los productos de tu negocio.
+            También te permite registrar y gestionar las ventas tanto
+            fisicas como virtuales. Si tu empresa ya esta usando Control Sales,
+            accede desde el botón de la derecha con tu cuenta de correo de
+            Google.
           </span>
         </div>
         <img src={logo} className="App-logo" alt="logo" />
