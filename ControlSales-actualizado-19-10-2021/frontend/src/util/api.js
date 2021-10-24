83f2bf30c6ae79cde67f7c2fb70be7dfdebff6cc
[alex] <[alxscorpic19@gmail.com]>
2021-10-24 12:34:26 -0500

    se actualizan tanto el back como el front con autenticacion

--- ControlSales-actualizado-19-10-2021/frontend/src/util/api.js
+++ ControlSales-actualizado-19-10-2021/frontend/src/util/api.js
@@ -4,9 +4,6 @@ const getToken = () => {
   return `Bearer ${localStorage.getItem('token')}`;
 };
 
-
-
-
  //CRUD DE PRODUCTOS
 export const registrarProductos = async (data, successCallback, errorCallback) => {
     const options = {
@@ -24,6 +21,40 @@ export const crearVenta = async (data, successCallback, errorCallback) => {
   const options = {
     method: 'POST',
     url: 'http://localhost:5000/ventas',
+    headers: { 'Content-Type': 'application/json', Authorization: getToken() },
+    data,
+  };
+  await axios.request(options).then(successCallback).catch(errorCallback);
+};
+
+// CRUD PARA USUARIOS
+
+export const obtenerUsuarios = async (successCallback, errorCallback) => {
+  const options = {
+    method: 'GET',
+    url: 'http://localhost:5000/usuarios',
+    headers: {
+      Authorization: getToken(),
+    },
+  };
+  await axios.request(options).then(successCallback).catch(errorCallback);
+};
+
+export const obtenerDatosUsuario = async (successCallback, errorCallback) => {
+  const options = {
+    method: 'GET',
+    url: 'http://localhost:5000/usuarios/self',
+    headers: {
+      Authorization: getToken(), // 3. enviarle el token a backend
+    },
+  };
+  await axios.request(options).then(successCallback).catch(errorCallback);
+};
+
+export const editarUsuario = async (id, data, successCallback, errorCallback) => {
+  const options = {
+    method: 'PATCH',
+    url: `http://localhost:5000/usuarios/${id}/`,
     headers: { 'Content-Type': 'application/json', Authorization: getToken() },
     data,
   };
