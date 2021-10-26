1aaccb181c0d3128992648959fa5ac9848df98d1
ALVARO RUIZ MACIAS <90515869+alvarikoke79@users.noreply.github.com>
2021-10-25 21:50:39 -0500

    Update Login.jsx
    Ajustes de texto en interfaz de inicio de sesión. Líneas 11, 12, 13 y 14.

--- ControlSales-actualizado-19-10-2021/frontend/src/pages/Login.jsx
+++ ControlSales-actualizado-19-10-2021/frontend/src/pages/Login.jsx
@@ -8,11 +8,11 @@ const Login = () => {
     return (
         <div className = 'mainLogin'>
            <div className = 'txtLogin'>
-            <span className='txtlanding'>Control sales te permite registrar, y gestionar los produtos de tu
-            tienda, tambien permite registrar y gestionar las ventas tanto
-            fisicas como virtuales. Si tu empresa ya esta usando control sales,
-            accede desde el boton de la derecha con tu cuenta de correo de
-            google.</span>
+            <span className='txtlanding'>Control sales te permitirá registrar y gestionar los produtos de tu
+            tienda. También podrás registrar y gestionar las ventas tanto
+            fisicas como virtuales. Si tu empresa ya esta usando Control Sales,
+            haz click al botón Iniciar Sesión y accede con tu cuenta de correo de
+            Google.</span>
            </div>
            <div className = 'mainform'>
            <img className='logologin' src={Logo}  alt="logo" />
